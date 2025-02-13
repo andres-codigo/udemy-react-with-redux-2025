1a6d90c3e6cc9d8fc829c2dde69d3a34160aaa31
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-02-13 15:32:11 +1100

    [project] phase 3: book list communication using the context system
    - context system
    - application vs component state and determining where to use state
    - props and context together
    - context creation
    - simple hooks introduction to use with context system

--- section-08-book-list-phase-03/src/index.js
+++ section-08-book-list-phase-03/src/index.js
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+import { Provider } from './context/books';
 import App from './App.js';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<App />
+		<Provider>
+			<App />
+		</Provider>
 	</StrictMode>,
 );
