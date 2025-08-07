132f82d7aec16f77a70e8192e92121c85b080ab9
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-08-07 16:05:08 +1000

    Allow console.log in ESLint config temporarily
    Added 'log' to the allowed methods for the 'no-console' rule to permit usage of console.log. This is a temporary change and will be reverted after the section is completed.

--- section-10-to-13-navigation-and-routing/eslint.config.mjs
+++ section-10-to-13-navigation-and-routing/eslint.config.mjs
@@ -61,7 +61,7 @@ export default [
 			],
 			'no-unused-vars': 'warn',
 			'react/prop-types': 'off',
-			'no-console': ['error', { allow: ['warn', 'error'] }],
+			'no-console': ['error', { allow: ['warn', 'error', 'log'] }], // TODO: remove 'log' from array one section completed
 			'no-debugger': 'warn',
 			'prettier/prettier': 'error',
 			quotes: ['error', 'single'],
